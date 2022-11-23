dbcea30c8c0b7347634a0a3e09d04b5f874b6413
Dinsky21 <dinsky21@gmail.com>
2022-11-23 18:01:07 +0800

    feat: show same shortURL with same url, prevent empty or only space input

--- routes/modules/shortURL.js
+++ routes/modules/shortURL.js
@@ -5,12 +5,27 @@ const generateCode = require('../../public/javascripts/urlTransform')
 const PORT = process.env.PORT || 3000
 
 router.post('/', (req, res) => {
-  const url = req.body.url
-  const shortURL = `http://${req.hostname}:${PORT}/${generateCode(url)}`
-  const web = { url, shortURL }
-  return URLlist.create({ url, shortURL })
-    .then(res.render('shortURL', { web }))
-    .catch((error) => console.log(error))
+  const { url } = req.body
+  if (url.trim() === '') {
+    res.redirect('/')
+  } else {
+    return URLlist.find()
+      .lean()
+      .then((arr) => {
+        const isInDB = arr.filter((e) => e.url.includes(url))
+        if (isInDB.length >= 1 || url === '') {
+          res.render('shortURL', { web: isInDB[0] })
+        } else {
+          const code = generateCode(url)
+          const shortURL = `http://${req.hostname}:${PORT}/${code}`
+          const web = { url, shortURL }
+          URLlist.create({ url, shortURL }).then(
+            res.render('shortURL', { web })
+          )
+        }
+      })
+      .catch((error) => console.log(error))
+  }
 })
 
 module.exports = router
